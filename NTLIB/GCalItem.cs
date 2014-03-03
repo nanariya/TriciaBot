11d4a90e27741154af545e85db2cadbbc22e09d1
nanariya <sieben.riya@gmail.com>
2014-03-04 08:24:18 +0900

    追加

---
+++ NTLIB/GCalItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTLIB
+{
+    public class GCalItem
+    {
+        public String Title { get; set; }
+        public bool AllDay { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public String Location { get; set; }
+        public List<String> GuestsEmail { get; set; }
+    }
+}
