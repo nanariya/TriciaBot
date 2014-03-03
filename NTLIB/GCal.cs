ec429d8d2855be07af8cdf0a49c35f23c7609c75
nanariya <sieben.riya@gmail.com>
2014-03-03 20:01:38 +0900

    ぐーぐるかれんだ

--- NTLIB/GCal.cs
+++ NTLIB/GCal.cs
@@ -12,11 +12,14 @@ namespace NTLIB
     public class GCal
     {
         private CalendarService _gCal { get; set; }
+        private String _gmailID { get; set; }
 
         public GCal(String GmailID, String GmailPass)
         {
             this._gCal = new CalendarService("nanariya-tool-engine");
             _gCal.setUserCredentials(GmailID, GmailPass);
+            _gmailID = GmailID;
+
         }
 
         public void WriteSchedule(String title, String description, String location, DateTime startTime, DateTime endTime)
@@ -24,8 +27,19 @@ namespace NTLIB
             EventEntry entry = new EventEntry(title, description, location);
             When eventTime = new When(startTime, endTime);
             entry.Times.Add(eventTime);
-            Uri url = new Uri("https://www.google.com/calendar/feeds/tricia.disraeli@gmail.com/private/full");
+            Uri url = new Uri("https://www.google.com/calendar/feeds/" + this._gmailID + "/private/full");
             AtomEntry result = _gCal.Insert(url, entry);
+        }
+        public void ReadSchedule()
+        { 
+            EventQuery query = new EventQuery();
+            query.Uri = new Uri("https://www.google.com/calendar/feeds/" + this._gmailID + "/private/full");
+            EventFeed feeds = _gCal.Query(query);
+            IEnumerable<EventEntry> entries = feeds.Entries.Cast<EventEntry>();
+            foreach (EventEntry entry in entries)
+            {
+                
+            }
         }
     }
 }
