b7bf7ef4c29b804b3c26f29d43ba431a76379d39
nanariya <sieben.riya@gmail.com>
2014-03-05 14:42:30 +0900

    風景を投稿するアレ

---
+++ TriciaBot/Proc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriciaBot
+{
+    public static class Proc
+    {
+        //定時動作
+        public static void Start(String[] args)
+        {
+            if(args[0].Substring(0,1).Equals("/"))
+            {
+                switch (args[0].Substring(1,1))
+                {
+                    case "h" :
+                        break;
+                    case "m" :
+                        switch(args[1])
+                        {
+                            case "PostPanoramio" :
+
+                                break;
+                            default :
+                                break;
+                        }
+                        break;
+                    default :
+                        break;
+                }
+            }
+        }
+
+        private static void PostPanoramio()
+        {
+
+        }
+
+
+        private static void LoopHour()
+        {
+
+        }
+        private static void LoopDay()
+        {
+
+        }
+    }
+}
--- TriciaBot/Program.cs
+++ TriciaBot/Program.cs
@@ -3,20 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace TriciaBot
 {
     static class Program
     {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AttachConsole(uint dwProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(String[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (args.Length > 0)
+            {
+                if(AttachConsole(System.UInt32.MaxValue))
+                {
+                    try
+                    {
+                        System.IO.StreamWriter stdout = new System.IO.StreamWriter(System.Console.OpenStandardOutput(),
+                            System.Text.Encoding.GetEncoding("shift-jis"));
+                        stdout.AutoFlush = true;
+                        System.Console.SetOut(stdout);
+
+                        Proc.Start(args);
+                        FreeConsole();
+                    }
+                    finally
+                    {
+                        //FreeConsole();
+                        //Environment.Exit(0);
+                    }
+                }
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
         }
     }
 }
