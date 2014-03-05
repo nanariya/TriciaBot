5dc3b7b8b5bce966b4694af378c14d18a6c7c7e5
nanariya <sieben.riya@gmail.com>
2014-03-05 17:50:12 +0900

    Panoramioから引っ張ってくるとこ

---
+++ NTLIB/Panoramio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace NTLIB
+{
+    public class Panoramio
+    {
+        public Panoramio()
+        {
+        }
+
+        public String GetRandomPhoto(Double maxx, Double maxy, Double minx, Double miny)
+        {
+            Double xdiff = maxx - minx;
+            Double ydiff = maxy - miny;
+            System.Random random = new Random();
+            Double r = random.NextDouble();
+
+            Double xr_min = minx + (xdiff * r);
+            Double yr_min = miny + (ydiff * r);
+
+            String jsonResult = "";
+            String url = "http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=full&from=0&to=10&minx=" + xr_min.ToString() + "&miny=" + yr_min.ToString() + "&maxx=" + (xr_min+1).ToString() + "&maxy=" + (yr_min+1).ToString() + "&size=medium";
+            WebRequest req = WebRequest.Create(url);
+            WebResponse res = req.GetResponse();
+            using(Stream stm = res.GetResponseStream())
+            {
+                using(StreamReader sr = new StreamReader(stm, System.Text.Encoding.GetEncoding("utf-8")))
+                {
+                    jsonResult = sr.ReadToEnd();
+                }
+            }
+            var result = Newtonsoft.Json.JsonConvert.DeserializeObject<PanoramioResult>(jsonResult);
+
+            return result.photos[random.Next(10)].photo_url;
+        }
+    }
+}
---
+++ NTLIB/PanoramioResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTLIB
+{
+    public class PanoramioResult
+    {
+        public int count { get; set; }
+        public bool has_more { get; set; }
+        public MapLocation map_location { get; set; }
+        public List<Photo> photos { get; set; }
+
+        public class MapLocation
+        {
+            public double lat { get; set; }
+            public double lon { get; set; }
+            public int panoramio_zoom { get; set; }
+        }
+
+        public class Photo
+        {
+            public int height { get; set; }
+            public double latitude { get; set; }
+            public double longitude { get; set; }
+            public int owner_id { get; set; }
+            public string owner_name { get; set; }
+            public string owner_url { get; set; }
+            public string photo_file_url { get; set; }
+            public int photo_id { get; set; }
+            public string photo_title { get; set; }
+            public string photo_url { get; set; }
+            public string upload_date { get; set; }
+            public int width { get; set; }
+            public string place_id { get; set; }
+        }
+    }
+}
